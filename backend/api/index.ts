9937847d3b0cfc4b4670c9d2217bd2ba483e8d88
Dilan2582 <descobarr9@miumg.edu.gt>
2025-10-28 20:28:55 -0600

    update2 api/index.ts backend

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -2,10 +2,7 @@
 import serverless from "serverless-http";
 import app from "../src/app";
 
-// Para uploads grandes, ya llevas tus parsers en app.ts,
-// así que desactiva el bodyParser de Vercel:
-export const config = {
-  api: { bodyParser: false }
-};
+export const config = { api: { bodyParser: false } };
 
-export default serverless(app);
+export default serverless(app, { basePath: "/api" });
+
